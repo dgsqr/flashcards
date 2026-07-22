a386a5ff35f77aff26bc209bedd5f043b6422dd0
rezird <148511997+rezird@users.noreply.github.com>
2026-07-22 20:07:11 -0300

    fix: app routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,8 +10,24 @@ import Demo from "./components/Demo";
 import Profile from "./components/Profile";
 import Create from "./components/Create";
 import DeckSettings from "./components/DeckSettings";
+import { useLayoutEffect } from "react";
+import axios from "axios";
 
 function App() {
+  useLayoutEffect(() => {
+    async function wakeupServer() {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/ping`,
+        );
+        console.log(response.data);
+      } catch (error: any) {
+        console.log(error);
+      }
+    }
+    wakeupServer();
+  }, []);
+
   return (
     <>
       <BrowserRouter>
